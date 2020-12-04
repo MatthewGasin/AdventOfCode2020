f8ee94b31c25d373bce25b43a9974c8811a2e78a
Matthew Gasin <taranack@gmail.com>
2020-12-03 20:27:00 -0800

    Updated template to support both parts

--- src/Template.java
+++ src/Template.java
@@ -5,13 +5,20 @@ public class Template {
 
     public static void main(String[] args) {
         String[] input = DataHelper.getInput("\n", 0);
+        partOne(input);
+        partTwo(input);
+    }
+
+    public static void partOne(String[] input){
         for (String data : input) {
 
         }
     }
 
-    public static void partOne(String[] input){
+    public static void partTwo(String[] input){
+        for (String data : input) {
 
+        }
     }
 
 
